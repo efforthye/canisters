c14bc2bbacab38671075f1c5d911831e743cb235
efforthye <efforthye@gmail.com>
2023-10-19 12:38:07 +0900

    feat: test web

---
+++ web_test/web/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+function App() {
+    return (
+        <>
+            <div className="App">
+                <span>하이</span>
+            </div>
+        </>
+    );
+}
+
+export default App;
